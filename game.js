97e5ff26b2e5485ccdc56c71d3b8f3df3564091d
Lsuihua <37467467+Lsuihua@users.noreply.github.com>
2020-11-17 16:10:28 +0800

    半吊子动画。

--- game.js
+++ game.js
@@ -16,6 +16,8 @@ class Game {
     this.textData = [];
     this.particleList = [];
     this.step = 8;
+    this.dx = Math.floor(Math.random()*10);
+    this.dy = Math.floor(Math.random()*10);
   }
 
   init(){
@@ -40,44 +42,79 @@ class Game {
     for (var y = 0; y < this.height; y += this.step) {
       for (var x = 0; x < this.width; x += this.step) {
           var opacityIndex = (x + y * this.width) * 4;
+
+          // 屏幕随机开始位子
+          var sx = this.width/2,
+              sy = this.height -50;
           if (this.textData[opacityIndex] > 0) {
               this.particleList.push(new Particle({
-                  x,
-                  y,
-                  r:3,
-                  color: '#fff'
+                sx,
+                sy,
+                cx:sx,
+                cy:sy,
+                ex:x,
+                ey:y,
+                r:3,
+                color: '#fff'
               }));
           }
       }
     }
+
+    this.animate();
   }
 
   change(options){
     if(options.color){
       this.color = options.color;
     }
-    this.text = options.text;
+    if(this.text){
+      this.text = options.text;
+    }
     if(options.step){
       this.step = options.step;
     }
-  }
-
-  update(){
     this.init();
   }
 
+  update(){
+    for(let i=0;i<this.particleList.length;i++){
+      // 速度随机
+      var dx = Math.floor(Math.random()*10),
+          dy = Math.floor(Math.random()*6);
+      // 计算当前位置
+      if(this.particleList[i].ex > this.particleList[i].cx){
+        this.particleList[i].cx += dx
+      }else if(this.particleList[i].ex < this.particleList[i].cx){
+        this.particleList[i].cx -= dx;
+      }else if(parseInt(this.particleList[i].ex - this.particleList[i].cx)<5){
+        this.particleList[i].cx = this.particleList[i].ex;
+      }
+
+      if(this.particleList[i].ey > this.particleList[i].cy){
+        this.particleList[i].cy += dy
+      }else if(this.particleList[i].ey < this.particleList[i].cy){
+        this.particleList[i].cy -= dy;
+      }else if(parseInt(this.particleList[i].ey - this.particleList[i].cy)<5){
+        this.particleList[i].cy = this.particleList[i].ey;
+      }
+    }
+  }
+
   render(){
-    for(let partic of this.particleList){
-      partic.update();
-      partic.draw();
+    for(let i=0;i<this.particleList.length;i++){
+      var cx = this.particleList[i].cx,
+          cy = this.particleList[i].cy;
+      this.particleList[i].update({cx,cy});
+      this.particleList[i].draw();
     }
   }
 
   animate(){
     var self = this;
     self.ctx.clearRect(0,0,self.width,self.height);
-    this.update();
-    this.render();
-    requestAnimationFrame(this.animate.bind(self));
+    self.update();
+    self.render();
+    requestAnimationFrame(self.animate.bind(self));
   }
 }
--- particle.js
+++ particle.js
@@ -1,7 +1,19 @@
 class Particle{
   constructor(options){
-    this.x = options.x || 0;
-    this.y = options.y || 0;
+    /**
+     * xs | sy 起始坐标 xy
+     * cx | cy 当前坐标 xy
+     * ex | ey 结束坐标 xy ==> 这个像素在图片文本中的原始位置
+     */
+
+    this.sx = options.sx || 0;
+    this.sy = options.sy || 0;
+    this.cx = options.cx || 0;
+    this.cy = options.cy || 0;
+    this.ex = options.ex || 0;
+    this.ey = options.ey || 0;
+    this.dx = options.dx || 0;
+    this.dy = options.dy || 0;
     this.r = options.r || 4;
     this.color = options.color || '#fff';
   }
@@ -9,14 +21,15 @@ class Particle{
   draw(){
     game.ctx.beginPath();
     game.ctx.fillStyle = this.color;
-    game.ctx.arc(this.x,this.y,this.r,0,Math.PI*2,false);
+    game.ctx.arc(this.cx,this.cy,this.r,0,Math.PI*2,false);
     game.ctx.fill();
     game.ctx.closePath();
   }
 
-  update(){
-    // this.x = this.x + Math.random() * 6 - 3;
-    // this.y = this.y + Math.random() * 6 - 3;
-    // this.r = 3 + 2 * Math.sin(new Date() / 1000 % 1000 * this.r);
+  update(options){
+    if(options){
+      this.cx = options.cx;
+      this.cy = options.cy
+    }
   }
 }
